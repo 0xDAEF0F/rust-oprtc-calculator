ecf93db6bcbc6ac745793feacc90131f6d4cfb2c
daemon <0xDAEF0F@proton.me>
2023-07-16 09:56:53 -0600

    subscribed to all events. changed to wss

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,44 @@
-use ethers::prelude::*;
+use dotenv::dotenv;
+use ethers::{
+    contract::abigen,
+    core::types::Address,
+    providers::{Provider, StreamExt, Ws},
+};
+use eyre::Result;
+use std::env;
+use std::sync::Arc;
 
-const RPC_URL: &str = "https://eth.llamarpc.com";
+abigen!(LendingVault, "LendingVault.json");
+
+const VAULT_ADDRESS: &str = "0xaF53431488E871D103baA0280b6360998F0F9926";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let provider = Provider::<Http>::try_from(RPC_URL)?;
-    let block_number: U64 = provider.get_block_number().await?;
+    dotenv().ok();
 
-    println!("{block_number}");
+    let wss_url = env::var("WSS_URL").expect("Env: WSS_URL");
+
+    let provider = Provider::<Ws>::connect(wss_url).await?;
+    let client = Arc::new(provider);
+    let address: Address = VAULT_ADDRESS.parse()?;
+    let contract = LendingVault::new(address, client);
+
+    listen_all_events(&contract).await?;
+
+    Ok(())
+}
+
+async fn listen_all_events(contract: &LendingVault<Provider<Ws>>) -> Result<()> {
+    let events = contract.events().from_block(17564663);
+    let mut stream = events.stream().await?.take(1);
+
+    while let Some(Ok(evt)) = stream.next().await {
+        match evt {
+            LendingVaultEvents::DepositFilter(f) => println!("{f:?}"),
+            LendingVaultEvents::WithdrawFilter(f) => println!("{f:?}"),
+            _ => println!("dont care."),
+        }
+    }
 
     Ok(())
 }
